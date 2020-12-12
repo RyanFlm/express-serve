c366bdd42a05e254d24bc0cbd7450c0e79142e11
RyanFlm <info@apelma.de>
2020-12-12 18:13:24 +0100

    Just use the get method for serve

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const app = require('express')();
 const handler = require('serve-handler');
 const port = parseInt(process.env.PORT) || 3000;
 
-app.all("*", (req, res) => handler(req, res, {public: "public"}));
+app.get("*", (req, res) => handler(req, res, {public: "public"}));
 app.listen(port, () => console.log(`Running at http://localhost:${port}`));
